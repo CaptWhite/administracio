a89749217e1dca2ead9f1e327eb416d6f3faf6c4
captwhite <captwhite57@gmail.com>
2026-05-02 01:20:40 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const nextConfig = {
   output: 'standalone',
 }
 
+const nextConfig = {
+  // Si existe la variable APP_BASE_PATH, la usa. Si no, usa raíz /
+  basePath: process.env.APP_BASE_PATH || '',
+}
+
 module.exports = nextConfig
